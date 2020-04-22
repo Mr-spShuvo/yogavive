5bfe896e51d1d2323e6b174d5318652d58778417
Shahriyar Parvez Shuvo <mr.shahriyarshuvo@gmail.com>
2020-04-23 00:32:33 +0600

    polishing media-nav

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,17 @@ import "../styles/app.scss";
 //import "../images/sprite/icons.svg";
 
 
-let menuIcon = document.querySelector('#media-menu__nav-icon');
-let menuContent = document.querySelector('.media-nav__container');
-menuIcon.addEventListener('click', function(){
-    menuContent.classList.toggle('media-nav__container--isVisible');
+const mediaNavIcon = document.querySelector('#media-menu__nav-icon');
+const mediaNavIconSvg = document.querySelector('.media-menu__icon use');
+const mediaNavContent = document.querySelector('.media-nav__container');
+let iconChanged;
+mediaNavIcon.addEventListener('click', (e)=>{
+    if(iconChanged){
+        mediaNavIconSvg.setAttribute('xlink:href', "sprite/icons.svg#menu");
+        iconChanged = false;
+    } else{
+        mediaNavIconSvg.setAttribute('xlink:href', "sprite/icons.svg#cross");
+        iconChanged = true;
+    }
+    mediaNavContent.classList.toggle('media-nav__container--isVisible');
 });
