95ab5c3169c789b64325fcf603172b951977064b
Shahriyar Parvez Shuvo <mr.shahriyarshuvo@gmail.com>
2020-08-19 00:46:56 +0600

    complete about section

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,13 @@
-import '../styles/app.scss';
-import 'plyr/src/sass/plyr.scss';
+import Plyr from "plyr";
+import "../styles/app.scss";
+import "plyr/src/sass/plyr.scss";
 
-import MediaNav from './modules/MediaNav';
-import ScrollOnNav from './modules/ScrollOnNav';
-import ClassToggler from './components/ClassToggler';
+import MediaNav from "./modules/MediaNav";
+import ScrollOnNav from "./modules/ScrollOnNav";
+import ClassToggler from "./components/ClassToggler";
 
-const logo = require('../images/logo/logo.png');
-const logoWH = require('../images/logo/logo-white.png');
+const logo = require("../images/logo/logo.png");
+const logoWH = require("../images/logo/logo-white.png");
 
 // Mobile Navigation
 const mediaNav = new MediaNav();
@@ -17,7 +18,10 @@ const scrollOnNav = new ScrollOnNav(logo, logoWH);
 scrollOnNav.scrollOn();
 
 // Hide Broadcast on Scroll
-const BroadcastToggler = new ClassToggler('.broadcast', 'broadcast__slide-down');
+const BroadcastToggler = new ClassToggler(
+  ".broadcast",
+  "broadcast__slide-down"
+);
 BroadcastToggler.toggleOnScroll();
 
 // Webpack Config
@@ -25,5 +29,5 @@ if (module.hot) {
   module.hot.accept();
 }
 
-import Plyr from 'plyr';
-const player = new Plyr('#player');
+// Video Player
+const player = new Plyr("#player");
